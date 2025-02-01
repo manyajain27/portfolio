55d9fe8fe5b919520eba2f6ccf9c0191c47712b0
manyajain27 <jainmanya2701@gmail.com>
2025-02-01 19:32:29 +0530

    computer for mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -29,7 +29,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.75} // Reduced mobile scale from 0.7 to 0.5
+        scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -3.5, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -68,6 +68,9 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          enablePan={false}
+          enableRotate={!isMobile}  // Disable rotation on mobile
+          touchEnabled={false}      // Disable touch controls
         />
         <Computers isMobile={isMobile} />
       </Suspense>
